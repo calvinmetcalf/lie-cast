ac23e08cfcb0fbb1d4c56a0b9701a6714d3c0e0f
Calvin Metcalf <cmetcalf@appgeo.com>
2013-12-16 10:06:15 -0500

    only "pass through" instances of lie

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 'use strict';
 var cast = require('../lib/cast');
 var resolve = require('lie-resolve');
+var promise = require('lie');
 require("mocha-as-promised")();
 var chai = require("chai");
 chai.should();
@@ -10,7 +11,12 @@ describe("cast",function(){
     it('should work with a value',function(){
         return cast(1).should.become(1);
     });
-    it('should work with a promise',function(){
-        return cast(resolve(1)).should.become(1);
+    // it('should work with a promise',function(){
+    //     return cast(resolve(1)).should.become(1);
+    // });
+    it('should work with casting',function(){
+      var fakePromise = { then: function() { } };
+      var casted = cast(fakePromise);
+      return casted.should.be.an.instanceOf(promise);
     });
 });
